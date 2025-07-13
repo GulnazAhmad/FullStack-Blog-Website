1e0a86b13e6a31f1e57e4a39ab8acbffe61fc8b5
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2025-07-13 17:35:43 +0530

    last attempt

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,5 @@
 //const URL = "http://localhost:8000";
 //export default URL;
 //export const IF = "http://localhost:8000/images/"; //making image folder public for the frontend to access
-export const URL = "http://35.180.97.86/:8000";
-export const IF = "http://35.180.97.86/:8000/images/";
+export const URL = "http://35.180.97.86:8000";
+export const IF = "http://35.180.97.86:8000/images/";
